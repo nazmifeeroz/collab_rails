7334ad66c69132f81202275c70b1a24e0d20ddf0
Nazmi <nazmifeeroz@hotmail.com>
2017-12-17 22:41:48 +0800

    - Add the name column to the users table -include name attribute to sign_up_params - methods instde the Regcontroller

--- db/migrate/20171217110122_devise_create_users.rb
+++ db/migrate/20171217110122_devise_create_users.rb
@@ -1,6 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      ## Name
+      t.string :name, null: false, default: ""
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
